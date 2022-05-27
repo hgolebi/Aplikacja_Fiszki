4efd87847d1ac8ca06c9a96eec670f4d8ffcfe1b
Hubert <hugo7744@gmail.com>
2022-05-27 19:02:51 +0200

    wyswietlanie fiszek, przycisk UCZ SIE

--- src/main/java/com/pap/fishes/papfishes/FishesScreenController.java
+++ src/main/java/com/pap/fishes/papfishes/FishesScreenController.java
@@ -6,16 +6,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Vector;
 
 public class FishesScreenController {
 
+    @FXML
+    Label term_label;
+    @FXML
+    Label category_label;
+    @FXML
+    Button ucz_sie_button;
+    @FXML
+    Button fish_button;
     private Stage stage;
     private Scene scene;
     private Parent root;
+    Fish currentFish;
+    Vector<Fish> fishList;
+
+    public FishesScreenController(){
+        fishList = QuerySender.getAllFishes();
+
+    }
 
     public void switchScene(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/fxml/fish.fxml"));
@@ -26,5 +43,33 @@ public class FishesScreenController {
     }
     public void OnMusicButtonClicked(){
         Main.muteMusic();
+    }
+    public void OnUczSieButtonClicked(){
+        String term;
+        String category;
+        if (!fishList.isEmpty()) {
+            currentFish = fishList.get(0);
+        }
+        else
+            currentFish = null;
+
+        if (currentFish == null){
+            term = "BRAK FISZEK";
+            category = "Dodaj fiszki uzywajac przycisku DODAJ";
+        }
+        else {
+            term = currentFish.getTerm();
+            category = currentFish.getCategory();
+        }
+        term_label.setText(term);
+        category_label.setText(category);
+//        ucz_sie_button.cancelButtonProperty();
+        ucz_sie_button.setVisible(false);
+    }
+    public void OnFishButtonClicked(){
+        if (!(currentFish == null)){
+            String definition = currentFish.getDefinition();
+            term_label.setText(definition);
+        }
     }
 }
